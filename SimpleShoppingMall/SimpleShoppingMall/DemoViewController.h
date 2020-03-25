5945d943eb24bc752949e1b6054ff477ee71744c
LuoYang <ydluo@thoughtworks.com>
2020-03-25 21:46:25 +0800

    add a demo vc

---
+++ SimpleShoppingMall/SimpleShoppingMall/DemoViewController.h
@@ -0,0 +1,17 @@
+//
+//  DemoViewController.h
+//  SimpleShoppingMall
+//
+//  Created by Yang Dev Luo on 2020/3/25.
+//  Copyright © 2020 Thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DemoViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SimpleShoppingMall/SimpleShoppingMall/DemoViewController.m
@@ -0,0 +1,34 @@
+//
+//  DemoViewController.m
+//  SimpleShoppingMall
+//
+//  Created by Yang Dev Luo on 2020/3/25.
+//  Copyright © 2020 Thoughtworks. All rights reserved.
+//
+
+#import "DemoViewController.h"
+
+@interface DemoViewController ()
+
+@end
+
+@implementation DemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#pragma -
+#pragma mark
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    return cell;
+}
+
+@end
